c7d988b8e5f58c37a9c7767bf7f84fb152436e5c
Joma Simeon <simjoms@gmail.com>
2018-10-16 10:11:25 +0300

    [#161204339] edited routes

--- api/app.py
+++ api/app.py
@@ -1,13 +1,13 @@
 from flask import Flask, make_response, jsonify, request
 from flask_restful import Resource, Api
-from api.resources import Product,Sales
+from api.resources import Product, Sales
 app = Flask(__name__)
 api = Api(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-api.add_resource(Product,'/api/v1/products')
-api.add_resource(Product,'/api/v1/products/<product_id>')
-api.add_resource(Sales,'/api/v1/sales')
-api.add_resource(Sales,'/api/v1/sales/<sales_id>')
+api.add_resource(Product, '/api/v1/products/')
+api.add_resource(Product, '/api/v1/products/<int:product_id/>')
+api.add_resource(Sales, '/api/v1/sales/')
+api.add_resource(Sales, '/api/v1/sales/<int:sales_id/>')
