9f8fc214538fb69acdbc6b1c65b0c100f6a8a55d
Joma Simeon <simjoms@gmail.com>
2018-10-16 10:09:01 +0300

    [#161204339] added add product resource

--- api/resources.py
+++ api/resources.py
@@ -1,12 +1,20 @@
+from flask import jsonify, make_response, request
 from flask_restful import Resource
+products = []
+
 
 class Product(Resource):
     def get(self):
         pass
-    
-    def post(self):
-        pass
 
+    def post(self):
+        data = request.get_json()
+        product = {'name': data['name'], 'category': data['category'],
+                   'description': data['description'],
+                   'price': data['price']
+                   }
+        products.append(product)
+        return make_response(jsonify({'products': products}), 201)
 
 
 class Sales(Resource):
