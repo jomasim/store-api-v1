e0eb856006292e9b50d8fd67938f4e6b693b08dd
Joma Simeon <simjoms@gmail.com>
2018-10-16 11:14:04 +0300

    [#161204339] fixed url malformation

--- api/app.py
+++ api/app.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 api.add_resource(Product, '/api/v1/products/')
-api.add_resource(Product, '/api/v1/products/<int:product_id/>')
+api.add_resource(Product, '/api/v1/products/<int:product_id>/')
 api.add_resource(Sales, '/api/v1/sales/')
-api.add_resource(Sales, '/api/v1/sales/<int:sales_id/>')
+api.add_resource(Sales, '/api/v1/sales/<int:sales_id>/')
