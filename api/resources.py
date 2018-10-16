1cf48f8d55c7649f2496dd5be7c286bda95819f2
Joma Simeon <simjoms@gmail.com>
2018-10-16 03:23:50 +0300

    [#161239678] added resource module

---
+++ api/resources.py
@@ -0,0 +1,17 @@
+from flask_restful import Resource
+
+class Product(Resource):
+    def get(self):
+        pass
+    
+    def post(self):
+        pass
+
+
+
+class Sales(Resource):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
