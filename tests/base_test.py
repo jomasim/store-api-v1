e0bc265740441dcaabe7daad760a25c7b2dd5abf
Joma Simeon <simjoms@gmail.com>
2018-10-16 09:18:44 +0300

    [#161242927] added base test cases

---
+++ tests/base_test.py
@@ -0,0 +1,19 @@
+import json
+import unittest
+from api.app import app
+
+
+class BaseTestCase(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client(self)
+
+    def get(self, url):
+        return self.client.get(url)
+
+    def post(self, url, data):
+        return self.client.post(url,
+                                data=json.dumps(data),
+                                content_type='application/json')
+
+    def tearDown(self):
+        pass
