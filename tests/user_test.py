ca5ce4ba7589de907b84238e04e0011b417b5494
Joma Simeon <simjoms@gmail.com>
2018-10-18 00:42:46 +0300

    [#161299056] added test for existing user

--- tests/user_test.py
+++ tests/user_test.py
@@ -6,12 +6,39 @@ class UserTestCase(BaseTestCase):
     def test_creating_user(self):
         sample_user = {
             'id': '1',
+            'name': 'john doe',
+            'email': 'doe@gmail.com',
+            'phone': '+254728109567',
+            'username': 'john'
+        }
+        existing_user = {
+            'id': '1',
             'name': 'joma simeon',
             'email': 'simjoms@gmail.com',
             'phone': '+254728109567',
-            'username':'joma'
+            'username': 'joma'
         }
-        response=self.post('/api/v1/user',sample_user)
+        response = self.post('/api/v1/user', sample_user)
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(json.loads(response.data),{'message':'user created successfully'})
-        self.assertEqual(response.mimetype,'application/json')
+        self.assertEqual(json.loads(response.data), {
+                         'message': 'user created successfully'})
+        self.assertEqual(response.mimetype, 'application/json')
+
+    def test_for_empty_data(self):
+        response = self.post('/api/v1/user', data={})
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(response.mimetype, 'application/json')
+
+    def test_for_existing_user(self):
+        existing_user = {
+            'id': '1',
+            'name': 'joma simeon',
+            'email': 'simjoms@gmail.com',
+            'phone': '+254728109567',
+            'username': 'joma'
+        }
+        response = self.post('/api/v1/user', existing_user)
+        self.assertEqual(response.status_code, 409)
+        self.assertEqual(json.loads(response.data), {
+                         'message': 'user exists'})
+        self.assertEqual(response.mimetype, 'application/json')
