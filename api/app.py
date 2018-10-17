9e93f0eed7c2bbe3b40089d743ed4cffc99729cf
Joma Simeon <simjoms@gmail.com>
2018-10-17 22:22:04 +0300

    [ft #161293560] added api blueprint

--- api/app.py
+++ api/app.py
@@ -1,14 +1,19 @@
-from flask import Flask, make_response, jsonify, request
+from flask import Flask,Blueprint
 from flask_restful import Resource, Api
 from api.resources import Product, Sales
-app = Flask(__name__)
-api = Api(app)
 
-api.add_resource(Product, '/api/v1/products/',
+api_blueprint=Blueprint("store-api",__name__,url_prefix='/api/v1')
+app = Flask(__name__)
+api=Api(api_blueprint)
+
+
+api.add_resource(Product, '/products/',
                  strict_slashes=False, endpoint='products')
-api.add_resource(Product, '/api/v1/products/<int:product_id>/',
+api.add_resource(Product, '/products/<int:product_id>/',
                  strict_slashes=False, endpoint='product_by_id')
-api.add_resource(Sales, '/api/v1/sales/',
+api.add_resource(Sales, '/sales/',
                  strict_slashes=False, endpoint='sales')
-api.add_resource(Sales, '/api/v1/sales/<int:sale_id>/',
+api.add_resource(Sales, '/sales/<int:sale_id>/',
                  strict_slashes=False, endpoint='sale_by_id')
+
+app.register_blueprint(api_blueprint)                 
