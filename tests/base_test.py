d8e5edce9a4c4ed7f4c5fff31ffc4b7a14f63877
Joma Simeon <simjoms@gmail.com>
2018-10-20 16:57:51 +0300

    [#161314587] fixed base test case test token

--- tests/base_test.py
+++ tests/base_test.py
@@ -6,18 +6,27 @@ from run import app
 class BaseTestCase(unittest.TestCase):
     def setUp(self):
         self.client = app.test_client(self)
+        url = "/api/v1/auth"
+        data = {"username": "joma", "password": "1234"}
+        response = self.client.post(url,
+                                    data=json.dumps(data),
+                                    content_type='application/json')
+        data=json.loads(response.data)
+        self.token="Bearer " + data ['access_token']
 
     def get(self, url):
         return self.client.get(url,
-        headers={"Authorization":"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1NDAwMzg2MDIsIm5iZiI6MTU0MDAzODYwMiwianRpIjoiNTdjNzQzZDQtMGNlMi00ZjlmLWE0NDItODc2OGQwNGM5NTA2IiwiZXhwIjoxNTQwMDM5NTAyLCJpZGVudGl0eSI6ImpvbWEiLCJmcmVzaCI6MTU0MDAzOTgwMiwidHlwZSI6ImFjY2VzcyJ9.cO-NroSHJQ1ITrV2yBJb6ulJDMIPZXObufly-L-GtpY"}
-        )
+                               headers={"Authorization": self.token}
+                               )
 
     def post(self, url, data):
         return self.client.post(url,
                                 data=json.dumps(data),
                                 content_type='application/json',
-                                headers={"Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1NDAwMzg2MDIsIm5iZiI6MTU0MDAzODYwMiwianRpIjoiNTdjNzQzZDQtMGNlMi00ZjlmLWE0NDItODc2OGQwNGM5NTA2IiwiZXhwIjoxNTQwMDM5NTAyLCJpZGVudGl0eSI6ImpvbWEiLCJmcmVzaCI6MTU0MDAzOTgwMiwidHlwZSI6ImFjY2VzcyJ9.cO-NroSHJQ1ITrV2yBJb6ulJDMIPZXObufly-L-GtpY"}
+                                headers={"Authorization": self.token}
                                 )
 
     def tearDown(self):
         pass
+
+        
