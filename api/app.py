ffe998c24eeb5337126aafb9472ba4fbfbfe5158
Joma Simeon <simjoms@gmail.com>
2018-10-20 11:17:41 +0300

    [#161314587] added flask_jwt_extended to app

--- api/app.py
+++ api/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, Blueprint
 from flask_restful import Resource, Api
+from flask_jwt_extended import JWTManager
 from api.resources import ProductController, SalesController, UserController
 from instance.api_config import api_config
+
 api_blueprint = Blueprint("store-api", __name__, url_prefix='/api/v1')
+jwt=JWTManager()
 
 ''' setting api config '''
 
@@ -11,6 +14,10 @@ def create_app(config_setting):
 
     app = Flask(__name__)
     app.config.from_object(api_config[config_setting])
+    app.config['JWT_SECRET_KEY']="SECRET KEY"
+
+
+    jwt.init_app(app)
 
     ''' setting api blueprint  '''
     api = Api(api_blueprint)
