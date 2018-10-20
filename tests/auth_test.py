7ccaffde8b44b70c2070bf06ded22cf7dff042a4
Joma Simeon <simjoms@gmail.com>
2018-10-20 14:45:02 +0300

    [#161314587] added auth tests

---
+++ tests/auth_test.py
@@ -0,0 +1,25 @@
+import json
+from tests.base_test import BaseTestCase
+from models.models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class TestAuth(BaseTestCase):
+    def test_for_get_user_identity(self):
+        user = User.get_by_username("joma")
+        self.assertTrue(user)
+        self.assertEqual(user['name'], "joma simeon")
+
+    def test_match_password(self):
+        username = "joma"
+        user = User.get_by_username(username)
+        password = "1234"
+        self.assertTrue(user)
+        self.assertTrue(check_password_hash(user['password'], password))
+
+    def test_login(self):
+        user={"username": "joma",
+            "password": "1234"
+                }
+        response = self.post('/api/v1/auth', data=user)
+        self.assertTrue(response)
+        self.assertEqual(response.status_code,200)
