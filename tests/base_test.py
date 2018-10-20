e714cd92a98ccaeb5df1426c51a9718bd2c779dd
Joma Simeon <simjoms@gmail.com>
2018-10-20 15:47:35 +0300

    [#161314587] added authorization headers to base test case

--- tests/base_test.py
+++ tests/base_test.py
@@ -8,12 +8,16 @@ class BaseTestCase(unittest.TestCase):
         self.client = app.test_client(self)
 
     def get(self, url):
-        return self.client.get(url)
+        return self.client.get(url,
+        headers={"Authorization":"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1NDAwMzg2MDIsIm5iZiI6MTU0MDAzODYwMiwianRpIjoiNTdjNzQzZDQtMGNlMi00ZjlmLWE0NDItODc2OGQwNGM5NTA2IiwiZXhwIjoxNTQwMDM5NTAyLCJpZGVudGl0eSI6ImpvbWEiLCJmcmVzaCI6MTU0MDAzOTgwMiwidHlwZSI6ImFjY2VzcyJ9.cO-NroSHJQ1ITrV2yBJb6ulJDMIPZXObufly-L-GtpY"}
+        )
 
     def post(self, url, data):
         return self.client.post(url,
                                 data=json.dumps(data),
-                                content_type='application/json')
+                                content_type='application/json',
+                                headers={"Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1NDAwMzg2MDIsIm5iZiI6MTU0MDAzODYwMiwianRpIjoiNTdjNzQzZDQtMGNlMi00ZjlmLWE0NDItODc2OGQwNGM5NTA2IiwiZXhwIjoxNTQwMDM5NTAyLCJpZGVudGl0eSI6ImpvbWEiLCJmcmVzaCI6MTU0MDAzOTgwMiwidHlwZSI6ImFjY2VzcyJ9.cO-NroSHJQ1ITrV2yBJb6ulJDMIPZXObufly-L-GtpY"}
+                                )
 
     def tearDown(self):
         pass
